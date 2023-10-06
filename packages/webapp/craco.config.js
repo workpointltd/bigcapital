d94270bf6b0b13ab713e81412d6eb10c85ed1e39
morgan4080 <morganmurungi@live.com>
2023-10-06 15:20:25 +0300

    changes

--- packages/webapp/craco.config.js
+++ packages/webapp/craco.config.js
@@ -1,9 +1,33 @@
-const path = require('path');
-
+const CracoAlias = require("craco-alias");
 module.exports = {
+plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        aliases: {
+          "@/components": "./src/components",
+          "@/interfaces": "./src/interfaces",
+	  "@/services": "./src/services",
+          "@/store": "./src/store",
+          "@/serviceWorker": "./src/serviceWorker",
+          "@/style": "./src/style",
+          "@/containers": "./src/containers",
+          "@/hooks": "./src/hooks",
+          "@/routes": "./src/routes",
+          "@/constants": "./src/constants",
+          "@/utils": "./src/utils",
+          "@/static": "./src/static",
+	}
+      }
+    }
+  ],
   webpack: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-};
+      configure: {
+        resolve: {
+          fallback: {
+            "path": require.resolve("path-browserify")
+          },
+        },
+      },
+   },
+}
